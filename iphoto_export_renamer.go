41e1e6977a58500def30e930dee9e20a38d55426
Greg Lavallee <greglavallee@wapolabs.com>
2015-04-23 22:22:54 -0400

    Checkpoint. Generating map sucessfully.
    TODO: Date converter
    TODO: Look at EXIF of files in DIR to figure out Dates
    TODO: Move functions to actually move folders

--- iphoto_export_renamer.go
+++ iphoto_export_renamer.go
@@ -1,57 +1,105 @@
 package main
 
 import (
-  "os"
-  "io/ioutil"
-  "github.com/codegangsta/cli"
+	"fmt"
+	"github.com/codegangsta/cli"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
+}
+
+func printType(a interface{}) {
+	fmt.Println(reflect.TypeOf(a))
+}
+
+func getDateBasedFilename(file os.FileInfo) string {
+	// Check if filename.Name() already starts with YYYMMDD and bail if so
+	/* isDate := blah(dir.Name())
+	   if someCheck {
+	     go equiv of next()
+	  }*/
+
+	// Check if dir.Name() converts easily to YYYMMDD in format
+	// 1 Apr YYYY or YYYY_MM_DD
+
+	/* getDateFromName := blah(dir.Name())
+	   if getDateFromName which returns a string then pass that to move
+	*/
+	fmt.Println("Processing " + file.Name())
+
+	return file.Name()
+}
+
+// Check map to see if key exists and increment if it already does
+func getUniqueMapKey(key string, mapToProcess map[string]string) string {
+	_, prs := mapToProcess[key]
+	if prs {
+		newKey := incrementKey(key)
+		return getUniqueMapKey(newKey, mapToProcess)
+	} else {
+		return key
+	}
+}
+
+// Look for _DD at the end. If it's already there,
+// increment it by 1. Use 0 padding
+func incrementKey(key string) string {
+	split := strings.Split(key, "_")
+	last := split[len(split)-1]
+	i, err := strconv.Atoi(last)
+	if err != nil {
+		return key + "_00"
+	} else {
+		last = zeroPad(i+1, 2)
+		return key + "_" + last
+	}
+}
+
+func zeroPad(i int, n int) string {
+	iAsString := strconv.Itoa(i)
+	return strings.Repeat("0", n-len(iAsString)) + iAsString
 }
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "iphoto_export_renamer"
-  app.Usage = "Rename a folder from iPhoto events export by date"
-  app.Action = func(c *cli.Context){
-    directory := "."
-    if len(c.Args()) > 0 {
-      directory = c.Args()[0]
-    }
+	app := cli.NewApp()
+	app.Name = "iphoto_export_renamer"
+	app.Usage = "Rename a folder from iPhoto events export by date"
+	app.Action = func(c *cli.Context) {
+		directory := "."
+		if len(c.Args()) > 0 {
+			directory = c.Args()[0]
+		}
 
-    println(directory)
-    dirList, err := ioutil.ReadDir(directory)
-    check(err)
-    for _, dir := range dirList{
-      // Check if dir.Name() already starts with YYYMMDD and bail if so
-      /* isDate := blah(dir.Name())
-       if someCheck {
-         go equiv of next()
-      }*/
+		fmt.Println("Processing Directory " + directory + "...\n")
 
-      // Check if dir.Name() converts easily to YYYMMDD in format
-      // 1 Apr YYYY or YYYY_MM_DD
+		dirList, err := ioutil.ReadDir(directory)
+		check(err)
+		var newDirList = make(map[string]string)
 
-      /* getDateFromName := blah(dir.Name())
-      if getDateFromName which returns a string then pass that to move
-      */
+		// Some vars we'll use in our loop
+		var fileName string
+		for _, file := range dirList {
 
-      // Go into directory, list files and repeat this loop to name
+			fileName = getDateBasedFilename(file)
+			fileName = getUniqueMapKey(fileName, newDirList)
+			newDirList[fileName] = file.Name()
 
-      // If no files are datelike, then try their exif data
+			// // Go into directory, list files and repeat this loop to name
 
-      println(dir.Name())
-    }
+			// // If no files are datelike, then try their exif data
 
-  }
+		}
 
-  app.Run(os.Args)
-}
+		fmt.Println(newDirList)
+	}
 
-
-func printHi() {
-  println("hi")
+	app.Run(os.Args)
 }
--- iphoto_export_renamer_test.go
+++ iphoto_export_renamer_test.go
@@ -2,9 +2,18 @@ package main
 
 import "testing"
 
+func TestGetDateBasedFilename(t *testing.T) {
 
-func TestDateChecker(t *testing.T){
-    var dirName string
+}
 
+func incrementKey(t *testing.T) {
+	// incrementKey "dummy_key_00" returns dummy_key_01
 
+	// incrementKey "dummy_key" returns dummy_key_00
+}
+
+func TestZeroPad(t *testing.T) {
+	// zeroPad(1, 2) returns "01"
+
+	// zeroPad(99, 2) returns "99"
 }
